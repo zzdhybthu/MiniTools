e90539e9ea1c73bfcd25ce159d6090a079d51e71
chenziyi <c-zy22@mails.tsinghua.edu.cn>
2024-03-12 22:38:43 +0800

    Make Zhihu Downloader more robost for markdown math formula

--- Zhihu2Markdown/processor.py
+++ Zhihu2Markdown/processor.py
@@ -63,14 +63,23 @@ class Processor:
 
 
 	def _process_markdown(self):
-   
-		pattern = r'\\\\\\'
-		self.markdown = re.sub(pattern, r'\\\\', self.markdown)
   
 		pattern = r'\$\$\n*z\n*z\n*d\n*h\n*y\n*b(.*?)b\n*y\n*h\n*d\n*z\n*z\n*\$\$'
 		def repl(matchobj):
 			return f'$$\n{matchobj.group(1)}\n$$'
 		self.markdown = re.sub(pattern, repl, self.markdown, flags=re.DOTALL)
+  
+		pattern = r'\\\\\\'
+		self.markdown = re.sub(pattern, r'\\\\', self.markdown)
+  
+		pattern = r'\\bm'
+		self.markdown = re.sub(pattern, r'\\boldsymbol', self.markdown)
+  
+		pattern = r'\\left\\\\'
+		self.markdown = re.sub(pattern, r'\\left\\', self.markdown)
+  
+		pattern = r'\\right\\\\'
+		self.markdown = re.sub(pattern, r'\\right\\', self.markdown)
   
 		with open(self.filename, 'w') as output_file:
 			output_file.write("# " + self.title + '\n\n')
